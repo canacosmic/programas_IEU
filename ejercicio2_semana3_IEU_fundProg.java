c810216fbf90c68f97c18879dd8a1938476544b2
Roy C <61641010+canacosmic@users.noreply.github.com>
2021-02-20 11:43:17 -0600

    finishing exercises
    ejercicios de la semana 3 IEU - fundamentos de programacion.

--- ejercicio2_semana3_IEU_fundProg.java
+++ ejercicio2_semana3_IEU_fundProg.java
@@ -1,7 +1,38 @@
 import java.util.Scanner;
 public class ejercicio2_semana3_IEU_fundProg {
+ static Scanner sc = new Scanner(System.in);
  public static void main(String[] args) {
-  
+  System.out.println("- Ingrese los siguientes datos solicitados del paciente -");
+
+  System.out.println("Ingresa un número de cama del paciente: ");
+  int cama = sc.nextInt();
+
+  System.out.println("Ingresa el nombre del paciente: ");
+  String nombre = sc.next();
+
+  System.out.println("Ingresa la edad del paciente: ");
+  int edad = sc.nextInt();
+
+  System.out.println("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
+  System.out.println("Indique el sintoma del paciente: ");
+  System.out.println(" Si tiene dolor de cabeza digite 1");
+  System.out.println(" Si tiene dolor de estomago digite 2");
+  int sintoma = sc.nextInt();
+
+  System.out.println("Indique la temperatura del paciente: ");
+  float temp = sc.nextFloat();
+
+  System.out.println("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
+  if (sintoma == 1 && temp > 38){
+   System.out.println("El paciente tiene gripa");
+  }
+  else if (sintoma == 2 && temp > 30){
+   System.out.println("El paciente tiene tifoidea");
+  }
+  else {
+   System.out.println("El paciente es dado de alta");
+  }
  }
- 
 }
---
+++ ejercicio3_semana3_IEU_fundProg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ejercicio3_semana3_IEU_fundProg {
+ static Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+  System.out.println("Ingresa un número entero de filas: ");
+  int filas = sc.nextInt();
+  System.out.println("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
+
+  for (int i = 1; i <= filas; i++){
+   for (int j = 1; j<= i; j++){
+    System.out.print(j + " ");
+   }
+   System.out.println();
+  }
+ }
+}
